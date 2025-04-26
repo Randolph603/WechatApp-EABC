cfa2ef1988baa5e84810f022f58b8b4b6d04c9a8
Randolph Liu <cliu603@gmail.com>
2025-04-26 22:54:57 +1200

    add @cloudbase/adapter-wx_mp and @cloudbase/js-sdk and set up

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,4 +1,6 @@
 import wxPromisify from "@Lib/promisify";
+import cloudbase from "@cloudbase/js-sdk"
+import adapter from "@cloudbase/adapter-wx_mp";
 
 // app.ts
 App<IAppOption>({
@@ -8,17 +10,36 @@ App<IAppOption>({
     const res = await LoginAsync();
     console.log(res.code);
 
-    const te = wxPromisify(wx.getUserInfo);
-    const aa = await te();
-    console.log(aa);
+    cloudbase.useAdapters(adapter);
 
-    const requestAsync = wxPromisify(wx.request);
-    const secret = '';
-    const result = await requestAsync({
-      url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx1d85f7df5bbea6d4&secret=' + secret + '&js_code=' + res.code + '&grant_type=authorization_code',
-      method: 'GET'
+    //移动应用安全来源  from https://tcb.cloud.tencent.com/dev?envId=prod-4glz11qiccc892f2#/env/safety-source
+    const app = cloudbase.init({
+      env: "prod-4glz11qiccc892f2",
+      clientId: "AAU5PwAB5WXwGEwSZjE",
     });
-    console.log(result);
+
+    const auth = app.auth();
+
+    auth.signInAnonymously().then(r => {
+      console.log(r)
+      const user = auth.currentUser;
+      console.log(user)
+      const db = app.database();
+      db.collection('Attendees')
+        .get()
+        .then(res => {
+          console.log(res)
+        });
+    });
+
+    // move to jscode2session on cloud function 
+    // const requestAsync = wxPromisify(wx.request);
+    // const secret = '';
+    // const result = await requestAsync({
+    //   url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx1d85f7df5bbea6d4&secret=' + secret + '&js_code=' + res.code + '&grant_type=authorization_code',
+    //   method: 'GET'
+    // });
+    // console.log(result);
 
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
