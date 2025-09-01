b98e2def3d9fead8b4091349f19742513dbcb9e1
Randolph Liu <cliu603@gmail.com>
2025-09-02 00:52:19 +1200

    add world rank

--- miniprogram/pages/rank/rank.ts
+++ miniprogram/pages/rank/rank.ts
@@ -1,12 +1,16 @@
+import { LoadMDRankAsync, LoadMSRankAsync, LoadWDRankAsync, LoadWSRankAsync, LoadXDRankAsync } from "@API/bwfService";
 import { SearchUsersForRankAsync } from "@API/userService";
-import { UpdateTabBarLaguage } from "@Lib/utils";
+import { ExcuteWithLoadingAsync, UpdateTabBarLaguage } from "@Lib/utils";
 
 Page({
   data: {
     selectedTab: 0,
     tabList: [{
-      id: 'general',
-      name: '个人',
+      id: 'MS',
+      name: '男单',
+    }, {
+      id: 'WS',
+      name: '女单',
     }, {
       id: 'MD',
       name: '男双',
@@ -18,22 +22,24 @@ Page({
       name: '混双',
     }],
     users: [],
-    rankList: [
-      { rank: 4, name: "Rosie Nash", score: 24324, avatar: "/images/rosie.png" },
-      { rank: 5, name: "Lucy Nguyen", score: 12324, avatar: "/images/lucy.png" },
-      { rank: 6, name: "Birdie Potter", score: 4324, avatar: "/images/birdie.png" },
-      { rank: 7, name: "Celia Brewer", score: 4324, avatar: "/images/celia.png" },
-      { rank: 9, name: "Lizzie Rhodes", score: 3324, avatar: "/images/lizzie.png" },
-    ]
+    worldMS: []
   },
 
   async onLoad() {
     UpdateTabBarLaguage();
+    await this.loalListAsync();
+  },
 
-    wx.showLoading({ title: 'Loading...' });
-    const users = await SearchUsersForRankAsync();
-    this.setData({ users: users });
-    wx.hideLoading();
+  async loalListAsync() {
+    await ExcuteWithLoadingAsync(async () => {
+      const users = await SearchUsersForRankAsync();
+      const worldMS = await LoadMSRankAsync();
+      const worldWS = await LoadWSRankAsync();
+      const worldMD = await LoadMDRankAsync();
+      const worldWD = await LoadWDRankAsync();
+      const worldXD = await LoadXDRankAsync();
+      this.setData({ users: users, worldMS, worldWS, worldMD, worldWD, worldXD, triggered: false });
+    });
   },
 
   onShareAppMessage() {
